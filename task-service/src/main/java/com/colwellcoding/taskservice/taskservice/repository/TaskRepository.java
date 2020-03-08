f663003bb006ecd158711a607ab92ce89bec23ed
unknown <colwell.russell.emerson@gmail.com>
2020-03-07 20:17:42 -0600

    Really removed this time

--- task-service/src/main/java/com/colwellcoding/taskservice/taskservice/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.colwellcoding.taskservice.taskservice.repository;
-
-import com.colwellcoding.taskservice.taskservice.entity.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task, Long> {
-}
