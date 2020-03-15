f2e8e5c4f3d99f72c7b053a23ff3701c37f353cf
unknown <colwell.russell.emerson@gmail.com>
2020-03-15 13:59:18 -0500

    added cc-netflix for service discovery

---
+++ cc-netflix/src/test/java/com/colwellcoding/util/ccnetflix/CcNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colwellcoding.util.ccnetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
